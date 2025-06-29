50abf215af21fe61e2b06545ef60f4d19d7bef2d
AnujMadhoria <anujmadhoria0105@gmail.com>
2025-06-29 05:56:33 +0530

    first commit

--- backend2/routes/geminiRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { geminiChat } = require('../controllers/geminiController');
-
-router.post('/chat', geminiChat);
-
-module.exports = router; 
